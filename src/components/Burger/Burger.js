035f296815674f6ea6f626f3b42179af212a3d87
Trevor Leffert <leffertt@gmail.com>
2021-03-19 15:16:06 -0500

    Dynamically building ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,25 @@ import styles from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            console.log(ingKey, props.ingredients[ingKey]);
+            // Appends BurgerIngredients for v number per key
+            return [...Array(props.ingredients[ingKey])].map((_, index) => {
+                return <BurgerIngredient key={ingKey + index} type={ingKey}/>
+            });
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+    if(!transformedIngredients.length) {
+        transformedIngredients = <p>Please add more ingredients</p>
+    }
+
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
