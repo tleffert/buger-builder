fa7ab4f14b426806dbaf66a7e11f06676491acd2
Trevor Leffert <leffertt@gmail.com>
2021-03-19 13:56:44 -0500

    Layout updates and init burgerbuilder

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import React, { Component } from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Layout />
+                <BurgerBuilder />
             </div>
         );
     }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 
+import styles from './Layout.module.css';
+
 const layout = ( props ) => (
     <Fragment>
         <div>Toolbar, sidedrawr, backdrop</div>
-        <main>
+        <main className={styles.content}>
             {props.children}
         </main>
     </Fragment>
