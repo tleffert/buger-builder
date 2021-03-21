bccbff916e6461f4d2d5d23b7fb4425c9a82d2ff
Trevor Leffert <leffertt@gmail.com>
2021-03-21 16:25:42 -0500

    added navitems to toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={styles.Toolbar}>
         <div>Menu</div>
         <Logo>logo</Logo>
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
