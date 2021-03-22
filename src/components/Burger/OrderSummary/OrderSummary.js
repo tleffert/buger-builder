4b7bf384e86964342b9dac61572708c35c57e135
Trevor Leffert <leffertt@gmail.com>
2021-03-22 13:09:27 -0500

    Optimizing modal interactions

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(ingKey => {
-            return (
-                <li key={ingKey}>
-                    <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
-                </li>
-            );
-        })
+    componentDidUpdate() {
+        console.log('ORDERSUMMARY - did update');
+    }
 
-    return (
-        <Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>Cancel</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>Continue</Button>
-        </Fragment>
-    );
+    render() {
+
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(ingKey => {
+                return (
+                    <li key={ingKey}>
+                        <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
+                    </li>
+                );
+            })
+
+        return (
+            <Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCanceled}>Cancel</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>Continue</Button>
+            </Fragment>
+        );
+    }
+
 };
 
-export default orderSummary;
+export default OrderSummary;
