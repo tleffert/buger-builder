e74312787564327db4f9643535c7555ee518725c
Trevor Leffert <leffertt@gmail.com>
2021-04-02 13:48:05 -0500

    refactoring some utils to shared context

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../components/withErrorHandler/withErrorHandler';
 import { purchaseBurger } from '../../../store/actions/index';
+import { checkValidity } from '../../../shared/utility';
 
 import styles from './ContactData.module.css';
 
@@ -110,29 +111,6 @@ class ContactData extends Component {
         this.props.onOrderBurger(order);
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-
-        if (!rules) {
-            return true;
-        }
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        return isValid;
-
-    }
-
     inputChangeHandler = (event, inputId) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -143,7 +121,7 @@ class ContactData extends Component {
         };
 
         updatedFormEle.value = event.target.value;
-        updatedFormEle.valid = this.checkValidity(updatedFormEle.value, updatedFormEle.validation);
+        updatedFormEle.valid = checkValidity(updatedFormEle.value, updatedFormEle.validation);
         updatedFormEle.touched = true;
         updatedOrderForm[inputId] = updatedFormEle;
 
