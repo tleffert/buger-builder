b0842bf9af6e6598cb04fd91cdd124b1c1f65c34
Trevor Leffert <leffertt@gmail.com>
2021-04-02 13:05:51 -0500

    Added route guaring based on authentication status

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { authCheckState } from './store/actions/index';
@@ -20,18 +20,39 @@ class App extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <Layout />
+
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth} />
+                <Route path="/" exact component={BurgerBuilder} />
+                <Redirect to="/" />
+            </Switch>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
                 <Switch>
                     <Route path="/checkout" component={Checkout} />
                     <Route path="/orders" component={Orders} />
-                    <Route path="/auth" component={Auth} />
                     <Route path="/logout" component={Logout} />
-                    <Route path="/" component={BurgerBuilder} />
+                    <Route path="/" exact component={BurgerBuilder} />
+                    <Redirect to="/" />
                 </Switch>
+            );
+        }
+
+        return (
+            <div>
+                <Layout />
+                {routes}
             </div>
         );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: !!state.auth.token
     }
 }
 
@@ -41,4 +62,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
